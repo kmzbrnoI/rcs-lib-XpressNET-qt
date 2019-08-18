16744d0203848f2ffa7df2beed93a12b28a6e22d
Jan Horacek <me@apophis.cz>
2019-08-18 14:15:45 +0200

    Signal edit: add button handlers.

--- src/form-signal-edit.cpp
+++ src/form-signal-edit.cpp
@@ -1,6 +1,43 @@
 #include "form-signal-edit.h"
 
+namespace SignalEdit {
+
 FormSignalEdit::FormSignalEdit(QWidget *parent)
 	: QDialog(parent) {
 	ui.setupUi(this);
+
+	QObject::connect(ui.b_apply, SIGNAL(released()), this, SLOT(b_apply_handle()));
+	QObject::connect(ui.b_storno, SIGNAL(released()), this, SLOT(b_storno_handle()));
+	QObject::connect(ui.b_temp_load, SIGNAL(released()), this, SLOT(b_temp_load_handle()));
+	QObject::connect(ui.b_delete_signal, SIGNAL(released()), this, SLOT(b_delete_signal_handle()));
+	QObject::connect(ui.b_add_signal, SIGNAL(released()), this, SLOT(b_add_signal_handle()));
+	QObject::connect(ui.b_temp_save, SIGNAL(released()), this, SLOT(b_temp_save	_handle()));
 }
+
+void FormSignalEdit::open(EditCallback callback) {
+	this->callback = callback;
+}
+
+void FormSignalEdit::open(RcsXn::XnSignal signal, EditCallback callback) {
+	this->callback = callback;
+}
+
+void FormSignalEdit::b_apply_handle() {
+}
+
+void FormSignalEdit::b_storno_handle() {
+}
+
+void FormSignalEdit::b_temp_load_handle() {
+}
+
+void FormSignalEdit::b_delete_signal_handle() {
+}
+
+void FormSignalEditb_add_signal_handle() {
+}
+
+void FormSignalEdit::b_temp_save_handle() {
+}
+
+} // namespace SignalEdit
--- src/form-signal-edit.h
+++ src/form-signal-edit.h
@@ -3,15 +3,33 @@
 
 #include <QMainWindow>
 #include "ui_signal-edit.h"
+#include "signals.h"
+
+namespace SignalEdit {
+
+using EditCallback = void (*)(RcsXn::XnSignal signal);
 
 class FormSignalEdit : public QDialog {
 	Q_OBJECT
 
 public:
 	FormSignalEdit(QWidget *parent = nullptr);
+	void open(EditCallback callback);
+	void open(RcsXn::XnSignal signal, EditCallback callback);
+
+private slots:
+	void b_apply_handle();
+	void b_storno_handle();
+	void b_temp_load_handle();
+	void b_delete_signal_handle();
+	void b_add_signal_handle();
+	void b_temp_save_handle();
 
 private:
 	Ui::f_signal_edit ui;
+	EditCallback callback;
 };
+
+} // namespace SignalEdit
 
 #endif // FORMSIGNALEDIT_H
